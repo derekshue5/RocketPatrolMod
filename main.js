602cbcdc15f3b05d903f179678c2f48cf7f6ff96
derekshue5 <63285290+derekshue5@users.noreply.github.com>
2020-04-22 15:42:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//POINTS : 25 PTS - Create new art for the explosion, spaceship and rocket assets
+//25 PTS - Create a smaller, faster spaceship worth more points.
+//50 PTS - Create a simultaneous 2-player mode.
+
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene:[ Menu, Play],
+};
+
+
+let game = new Phaser.Game(config);
+
+//define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
+
+let keyF, keyRIGHT, keyLEFT, keyD, keyA, keyK;
